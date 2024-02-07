f75015325ae72d04a03244be60850eff88ddbcc6
haziq-dev <haziqdev22@gmail.com>
2024-02-07 16:10:48 +0800

    Revert "commit 2"
    This reverts commit 4a946d0e2813f41e6b5a894eaeb8b8989f3697c3.

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -18,8 +18,6 @@ class LoginController extends Controller
         echo "changes on module 2 for revert";
         echo "changes on module 2 for revert";
         echo "changes on module 2 for revert";
-
-        echo "changes on module 2 for revert commit 2";
     }
 
     public function index()
