d9d5f6705faf9a203d987c3273e421e33312ae16
haziq-dev <haziqdev22@gmail.com>
2024-02-07 16:03:18 +0800

    soft reset

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,10 +6,16 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function create() {
+    public function create()
+    {
         echo "create method here";
+
+        echo "changes on module 2";
+        echo "changes on module 2";
+        echo "changes on module 2";
+        echo "soft reset on module 2";
     }
-    
+
     public function index()
     {
         echo "changes here";
