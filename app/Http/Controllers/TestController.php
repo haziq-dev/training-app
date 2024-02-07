3692034bc064b6a44039c98f7ce1591a9cb8bd0b
haziq-dev <haziqdev22@gmail.com>
2024-02-07 10:08:20 +0800

    git reset --soft HEAD~5

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        echo "some changes here";
+        // change 1
+        // change 2
+        // change 3
+        // change 4
+    }
+}
