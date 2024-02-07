9efa3b69d04bdd8e0ef9b99de697fd61854c0a49
haziq-dev <haziqdev22@gmail.com>
2024-02-07 15:04:58 +0800

    update on login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,10 +6,15 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function create() {
+    public function create()
+    {
         echo "create method here";
+
+        echo "added another line";
+        echo "added another line";
+        echo "added another line";
     }
-    
+
     public function index()
     {
         echo "changes here";
